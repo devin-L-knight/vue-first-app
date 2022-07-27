de0897b3a2827aad921be53b94f654bc7b673d72
Devin Knight <devin.knight@accenture.com>
2022-07-27 12:54:14 -0400

    updated main.js to import new component & add the component to the App using a unique custom element name ('friend-contact').

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// imported Components:
+import FriendContact from './components/FriendContact.vue';
+
+// we create our app here.
+const app = createApp(App);
+
+// we include our components to the app object here
+// the component method accepts two arguments...a string name of the component and an object (which is FriendContact)
+app.component('friend-contact', FriendContact);
+
+// we mount the app below the other logic.
+app.mount('#app')

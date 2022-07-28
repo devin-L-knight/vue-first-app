79ef906da9d7dfa7d660858b301d113bdf9b0ec3
Devin Knight <devin.knight@accenture.com>
2022-07-28 14:27:59 -0400

    updated the ActiveUser component to be able to receive expected props & pass the props dynamically to its template.

--- src/components/ActiveUser.vue
+++ src/components/ActiveUser.vue
@@ -1,15 +1,26 @@
 <template>
     <section>
-        <h2><strong>Name: </strong>USER NAME</h2>
-        <h3><strong>Age: </strong>USER AGE</h3>
+        <h2><strong>Name: </strong>{{ userName }}</h2>
+        <h3><strong>Age: </strong>{{ userAge }}</h3>
+        <h3><strong>USER ID: </strong>{{ userId }}</h3>
     </section>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-
+    // these are the props we accept from that are passed down from App.vue (where this data is defined / altered)
+    props: {
+        userId: {
+            type: Number,
+            required: true
+        },
+        userName: {
+            type: String,
+            required: true
+        },
+        userAge: {
+            type: Number,
+            required: true
         }
     }
 }

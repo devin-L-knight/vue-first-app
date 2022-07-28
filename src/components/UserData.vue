5b2dedc8eb1b912649041b0e9693829bdb819b31
Devin Knight <devin.knight@accenture.com>
2022-07-28 14:30:51 -0400

    updated UserData component to bind data from form inputs & submit the data using an emitter to transmit data back to App.vue when the form has been submitted (allows a new user to be created, etc.

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -1,14 +1,33 @@
 <template>
-    <form>
-        <input type="text" placeholder="Enter Name">
-        <input type="text" placeholder="Enger Age">
+<!-- use submit.prevent to prevent the default behavior from refreshing the entire screen -->
+    <form @submit.prevent='submitForm'>
+        <input type="text" placeholder="Enter Name" v-model='inputUserName'>
+        <input type="text" placeholder="Enger Age" v-model='inputUserAge'>
+        <button>Add User</button>
     </form>
 </template>
 
 <script>
 export default {
     data() {
-        return {}
+        return {
+            inputUserName: '',
+            inputUserAge: 0
+        }
+    }, 
+    methods: {
+        submitForm() {
+        // create a new object with the submitted data
+        const submittedData = {
+            userName: this.inputUserName,
+            userAge: parseInt(this.inputUserAge)
+        }
+        this.$emit('create-new-user', submittedData);   // transmit the info back to App.vue with the submitted data
+
+        // clear the form fields by resetting everything to original state
+        this.inputUserName = '';
+        this.inputUserAge = 0;
+        }
     }
 }
 </script>
